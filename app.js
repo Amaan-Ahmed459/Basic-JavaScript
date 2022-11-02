222377d0a479ecad932ee47aeef0c7b779ab6f05
Amaan-Ahmed459 <amaanferoze@gmail.com>
2022-11-02 03:22:31 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,108 @@
+alert ( "hello!" );
+var name = "Mark";
+var name = "Mark";
+name = "Ace";
+
+var nationality;
+nationality = "U.S.";
+var floogle = "Mark";
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+var nickname = "Bub";
+
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+
+
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+
+
+var weight = 150;
+
+weight + 25
+ var originalNum = 23;
+ var newNum = originalNum + 7;
+ var originalNum = 23;
+  var numToBeAdded = 7;
+  var originalNum = 90;
+   originalNum = originalNum + 10; 
+   var newNum = originalNum + numToBeAdded;
+   var originalNum = "23";
+    var newNum = originalNum + 7;
+
+    var originalNumber = 23;
+     var newNumber = originalNumber + 7;
+     var firstPresident = "Washington";
+     alert(144);
+     var caseQty = 144;
+alert(caseQty);
+
+
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+alert(2 + 2);
+var popularNumber = 12 - 24;
+var popularNumber = 3 * 12;
+var num = 10;
+var popularNumber = num + 200;
+var num = 10;
+ var anotherNum = 1;
+ var popularNumber = num + anotherNum;
+ var whatsLeftOver = 10 % 3;
+ var whatsLeftOver = 9 % 3;
+
+
+ num++;
+ num = num + 1;
+ num--;
+ var num = 1;
+var newNum = num++;
+var num = 1;
+var newNum = ++num;
+var num = 1;
+ var newNum = num--;
+
+ var num = 1;
+ var newNum = --num;
+
+ var totalCost = 1 + (3 * 4);
+ var totalCost = (1 + 3) * 4;
+ var resultOfComputation = (2 * 4) * 4 + 2;
+ resultOfComputation = ((2 * 4) * 4) + 2;
+ resultOfComputation = (2 * 4) * (4 + 2);
+
+
+
+ alert("Thanks for your input!");
+ var message = "Thanks for your input!";
+ alert(message);
+ alert("Thanks, " + userName + "!");
+ var message = "Thanks, ";
+ var banger = "!";
+ alert(message + userName + banger);
+ alert("Thanks, " + "Susan" + "!");
+
+ var message = "Thanks, ";
+ var userName = "Susan";
+ var banger = "!";
+ var customMess = message + userName + banger;
+ alert(customMess);
+ alert("2" + "2");
+ alert("2 plus 2 equals " + 2 + 2);
+
+
+console.log(name);
+console.log(nationality);
+console.log(newNum);
+console.log(popularNumber);
+console.log(newNum);
+console.log(resultOfComputation);
+console.log(customMess);
